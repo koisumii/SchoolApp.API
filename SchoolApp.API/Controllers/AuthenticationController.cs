5c6ea93fbaac77d6f44e6e930ee5480cc55fff31
koisumii <2256255@johnabbottcollege.net>
2024-11-10 16:24:34 -0500

    Add user registration with UserManager

--- SchoolApp.API/Controllers/AuthenticationController.cs
+++ SchoolApp.API/Controllers/AuthenticationController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using SchoolApp.API.Data;
 using SchoolApp.API.Data.Models;
+using SchoolApp.API.Data.ViewModels;
+using System;
+using System.Threading.Tasks;
 
 namespace SchoolApp.API.Controllers
 {
@@ -25,6 +28,34 @@ namespace SchoolApp.API.Controllers
             _roleManager = roleManager;
             _context = context;
             _configuration = configuration;
+        }
+
+        [HttpPost("register-user")]
+        public async Task<IActionResult> Register([FromBody]RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Please, provide all the required fields");
+            }
+
+            var userExists = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if(userExists != null)
+            {
+                return BadRequest($"User {registerVM.EmailAddress} already exists");
+            }
+
+            ApplicationUser newUser = new ApplicationUser()
+            {
+                FirstName = registerVM.FirstName,
+                LastName = registerVM.LastName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.UserName,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+            var result = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (result.Succeeded) return Ok("User created");
+            return BadRequest("User could not be created");
         }
     }
 }
